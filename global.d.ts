5b28b4982087bf295f145151eb7ad8f43ce2727e
Asish Binu Mathew <asishbm98@gmail.com>
2024-10-29 00:49:37 +0000

    ID

--- global.d.ts
+++ global.d.ts
@@ -2,7 +2,7 @@
 declare namespace NodeJS {
   interface ProcessEnv {
     NEXT_PUBLIC_GITHUB_API: string;
-    NEXT_PUBLIC_GA_FOUR: string;
+    NEXT_GA_MEASUREMENT_ID: string;
     NEXT_PUBLIC_DISCORD_WEBHOOK_URL: string;
     NEXT_PUBLIC_ENV: string;
   }
